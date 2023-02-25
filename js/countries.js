3c98b1aff2dfb8dc421e93826e811fed08b4da4b
antik1801 <antik.edu@gmail.com>
2023-02-25 09:47:05 +0600

    countries added

---
+++ js/countries.js
@@ -0,0 +1,18 @@
+const loadCountries =()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(response => response.json())
+    .then(data => displayCountries(data))
+}
+const displayCountries = countries =>{
+    const countriesContainer = document.getElementById('all-countries')
+    countries.forEach(country => {
+        console.log(country);
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country','text-dark','mx-4','px-4','mb-2','py-2');
+        countryDiv.innerHTML = `
+        <h3>Name: </h3> 
+        `
+        countriesContainer.appendChild(countryDiv);
+    });
+}
+loadCountries();
